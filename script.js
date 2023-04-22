40e0f0a74e4f455d49ace0e7371cd4cca8a6f52f
bhargavsai15 <36848893+bhargavsai15@users.noreply.github.com>
2023-04-22 18:49:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-function highlight() {
-    //Write your code here
+const link = document.querySelector('p>a');
 
+link.addEventListener('mouseover',highlight);
+link.addEventListener('mouseout', return_normal)
 
+// link.addEventListener('mouseout', return_normal);
+const boldElements = document.getElementsByTagName('strong');
+console.log(boldElements);
+function highlight(event) {
+  //Write your code here
+  for (let i = 0; i < boldElements.length; i++){
+    boldElements[i].style.color = 'lightgreen';
+  }
 }
 
 
 function return_normal() {
-    //Write your code here
-
-    
+  //Write your code here
+  for (let i = 0; i < boldElements.length; i++){
+    boldElements[i].style.color = 'black';
+  }
 }
